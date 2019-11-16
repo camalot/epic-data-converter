8a30687634b290f763f4e9c17edf367cd41d18a4
Ryan Conrad <camalot@gmail.com>
2019-11-15 18:43:55 -0600

    init

---
+++ convert.py
@@ -0,0 +1,39 @@
+import sys
+import json
+import os
+import re
+import random
+import datetime
+import glob
+import time
+import shutil
+import tempfile
+import click
+
+
+@click.command()
+@click.argument('infile', default=None)
+@click.argument('outfile', default=None)
+def main(infile, outfile):
+    epicData = list()
+    with open(infile) as json_in_file:
+        data = json.load(json_in_file)
+        for d in data['data']:
+            group = data['data'][d]
+            link = None
+            if group['link'] is None:
+                link = "https://store.epicgames.com/{{CREATORCODE}}/" + d
+            else:
+                link = group['link'].replace("/darthminos/", "/{{CREATORCODE}}/")
+            name = group['name']
+            icon = group['icon']
+            epicData.append({
+                "link": link,
+                "name": name,
+                "icon": icon
+            })
+    with open(outfile, 'w') as out_file:
+        json.dump(epicData, out_file)
+
+if __name__ == "__main__":
+    main()
